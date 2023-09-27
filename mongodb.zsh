45df32e27d94fe94b6cd86dafbbbe3dcef74a3aa
Felipe Gasper <felipe@felipegasper.com>
2023-09-27 10:59:28 -0400

    tools & private

--- mongodb.zsh
+++ mongodb.zsh
@@ -2,3 +2,6 @@ path=(~/go/src/github.com/10gen/mongosync/dev-bin $path)
 
 export MSDIR=$HOME/go/src/github.com/10gen/mongosync
 export MVDIR=$HOME/go/src/github.com/10gen/migration-verifier
+export MTDIR=$HOME/go/src/github.com/mongodb/mongo-tools
+
+export GOPRIVATE=github.com/10gen/
