1441cc49f13b472b8dfe18305eb6eb0b0c0d46d3
Felipe Gasper <felipe@felipegasper.com>
2023-11-07 10:13:00 -0500

    verifier move

--- mongodb.zsh
+++ mongodb.zsh
@@ -1,7 +1,7 @@
 path=(~/go/src/github.com/10gen/mongosync/dev-bin $path)
 
 export MSDIR=$HOME/go/src/github.com/10gen/mongosync
-export MVDIR=$HOME/go/src/github.com/10gen/migration-verifier
+export MVDIR=$HOME/go/src/github.com/mongodb-labs/migration-verifier
 export MTDIR=$HOME/go/src/github.com/mongodb/mongo-tools
 
 export GOPRIVATE=github.com/10gen/
